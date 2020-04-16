f0e3ad223f48fd46caa4821379b8cd62985863f9
Ravinder Saluja <ravz.saluja@yahoo.com>
2020-04-16 18:34:46 +0530

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Ravinder Saluja on 16/04/20.
+//  Copyright © 2020 Ravinder Saluja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
